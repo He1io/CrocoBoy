6f35395a66b545cf6e0a73b55ed9dafa76c6fa94
He1io <helio.ojeda.reyes@gmail.com>
2017-08-18 00:02:53 +0100

    Level 2-3
    - Finish designing level 2-3.
    
    - New enemy AI (YellowDragon).

---
+++ CrocoBoy/Assets/Scripts/YellowDragon.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class YellowDragon : Enemy
+{
+    public Transform currentPoint;
+
+    public Transform[] points;
+
+    public int pointSelection;
+
+    SpriteRenderer renderer;
+
+    void Start()
+    {
+        currentPoint = points[pointSelection];
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, Time.deltaTime * Speed);
+
+        if (transform.position == currentPoint.position)
+        {
+            pointSelection++;
+
+            //If the enemy gets to half of his path, flip the graphics 
+            if (pointSelection == 2)
+            {
+                renderer.flipX = !renderer.flipX;
+            }
+
+            if (pointSelection == points.Length)
+            {
+                pointSelection = 0;
+                renderer.flipX = !renderer.flipX;
+            }
+
+            currentPoint = points[pointSelection];
+        }
+    }
+}
